d414c93c9df010e65f37d24944227cddd4a3852d
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2022-05-23 12:06:48 +0300

    cs fix

--- src/files/client/modules/real-estate/src/views/real-estate-property/record/panels/matching-requests.js
+++ src/files/client/modules/real-estate/src/views/real-estate-property/record/panels/matching-requests.js
@@ -26,7 +26,10 @@
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-Espo.define('real-estate:views/real-estate-property/record/panels/matching-requests', 'views/record/panels/relationship', function (Dep) {
+define(
+    'real-estate:views/real-estate-property/record/panels/matching-requests',
+    'views/record/panels/relationship',
+    function (Dep) {
 
     return Dep.extend({
 
@@ -35,10 +38,9 @@ Espo.define('real-estate:views/real-estate-property/record/panels/matching-reque
         setup: function () {
             Dep.prototype.setup.call(this);
 
-            this.listenTo(this.model, 'sync', function () {
+            this.listenTo(this.model, 'sync', () => {
                 this.collection.fetch();
-            }, this);
-
+            });
         },
 
         setupActions: function () {
@@ -46,11 +48,13 @@ Espo.define('real-estate:views/real-estate-property/record/panels/matching-reque
 
             if (Dep.prototype.actionViewRelatedList) {
                 var index = -1;
-                this.actionList.forEach(function (item, i) {
-                    if (item.action == 'listMatching') {
+
+                this.actionList.forEach((item, i) => {
+                    if (item.action === 'listMatching') {
                         index = i;
                     }
-                }, this);
+                });
+
                 if (~index) {
                     this.actionList.splice(index, 1);
                 }
@@ -61,18 +65,21 @@ Espo.define('real-estate:views/real-estate-property/record/panels/matching-reque
             var id = data.id;
 
             var model = this.collection.get(id);
-            if (!model) return;
+
+            if (!model) {
+                return;
+            }
 
             this.notify('Please wait...');
 
-            this.getModelFactory().create('RealEstateRequest', function (model) {
+            this.getModelFactory().create('RealEstateRequest', (model) => {
                 model.id = id;
 
-                this.listenToOnce(model, 'sync', function () {
+                this.listenToOnce(model, 'sync', () => {
                     this.notify(false);
 
-
                     var attributes = {};
+
                     attributes.propertyId = this.model.id;
                     attributes.propertyName = this.model.get('name');
                     attributes.requestId = model.id;
@@ -89,47 +96,49 @@ Espo.define('real-estate:views/real-estate-property/record/panels/matching-reque
                     attributes.contactsNames = model.get('contactsNames') || {};
 
                     attributes.contactsColumns = {};
-                    contactIdList.forEach(function (id) {
+
+                    contactIdList.forEach((id) => {
                         attributes.contactsColumns[id] = {role: 'Requester'};
-                    }, this);
+                    });
 
                     var mContactIdList = this.model.get('contactsIds') || [];
                     var mContactNames = this.model.get('contactsNames') || {};
                     var mContactColumns = this.model.get('contactsColumns') || {};
 
-                    mContactIdList.forEach(function (id) {
+                    mContactIdList.forEach((id) => {
                         attributes.contactsIds.push(id);
                         attributes.contactsNames[id] = mContactNames[id] || 'Unknown';
                         attributes.contactsColumns[id] = {role: (mContactColumns[id] || {}).role || null};
-                    }, this);
+                    });
 
                     this.createView('modal', 'views/modals/edit', {
                         scope: 'Opportunity',
-                        attributes: attributes
-                    }, function (view) {
+                        attributes: attributes,
+                    }, (view) => {
                         view.render();
 
-                        this.listenTo(view, 'after:save', function () {
+                        this.listenTo(view, 'after:save', () => {
                             this.collection.fetch();
 
                             if (this.getParentView() && this.getParentView().getView('opportunities')) {
                                 this.getParentView().getView('opportunities').actionRefresh();
                             }
-                        }, this);
-                    }, this);
-
-                }, this);
+                        });
+                    });
+                });
 
                 model.fetch();
-
-            }, this);
+            });
         },
 
         actionSetNotInterested: function (data) {
             var id = data.id;
 
             var model = this.collection.get(id);
-            if (!model) return;
+
+            if (!model) {
+                return;
+            }
 
             model.set('interestDegree', 0);
 
@@ -138,9 +147,9 @@ Espo.define('real-estate:views/real-estate-property/record/panels/matching-reque
                 type: 'POST',
                 data: JSON.stringify({
                     propertyId: this.model.id,
-                    requestId: model.id
+                    requestId: model.id,
                 })
-            }).done(function () {
+            }).then(() => {
                 model.set('interestDegree', 0);
             });
         },
@@ -149,7 +158,10 @@ Espo.define('real-estate:views/real-estate-property/record/panels/matching-reque
             var id = data.id;
 
             var model = this.collection.get(id);
-            if (!model) return;
+
+            if (!model) {
+                return;
+            }
 
             model.set('interestDegree', null);
 
@@ -158,9 +170,9 @@ Espo.define('real-estate:views/real-estate-property/record/panels/matching-reque
                 type: 'POST',
                 data: JSON.stringify({
                     propertyId: this.model.id,
-                    requestId: model.id
+                    requestId: model.id,
                 })
-            }).done(function () {
+            }).then(() => {
                 model.set('interestDegree', null);
             });
         },
@@ -171,11 +183,11 @@ Espo.define('real-estate:views/real-estate-property/record/panels/matching-reque
 
         actionViewRelatedList: function (data) {
             data.viewOptions = {
-                listViewUrl: '#RealEstateProperty/listMatching?id=' + this.model.id
+                listViewUrl: '#RealEstateProperty/listMatching?id=' + this.model.id,
             };
+
             Dep.prototype.actionViewRelatedList.call(this, data);
-        }
+        },
 
     });
-
 });
