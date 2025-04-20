1a63aace0b933b9ffbfa8b717ce3730f4553b484
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2025-04-20 16:28:36 +0300

    cs

--- src/files/client/custom/modules/real-estate/src/views/real-estate-request/detail.js
+++ src/files/client/custom/modules/real-estate/src/views/real-estate-request/detail.js
@@ -26,22 +26,20 @@
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word.
  ************************************************************************/
 
-Espo.define('real-estate:views/real-estate-request/detail', 'views/detail', function (Dep) {
+define('real-estate:views/real-estate-request/detail', ['views/detail'], function (Dep) {
 
     return Dep.extend({
 
         setup: function () {
             Dep.prototype.setup.call(this);
 
-            if (['Completed', 'Canceled', 'Lost'].indexOf(this.model.get('status')) == -1) {
+            if (['Completed', 'Canceled', 'Lost'].indexOf(this.model.get('status')) === -1) {
                 this.menu.buttons.push({
                     label: 'Matching Properties',
                     name: 'matchingProperties',
                     link: '#RealEstateRequest/listMatching?id='+ this.model.id
                 });
             }
-        }
-
+        },
     });
 });
-
